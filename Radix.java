22d116682e0ec40c32a9688ddde66d71f5abaf21
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-13 23:19:53 +0000

    still need to fix

--- Radix.java
+++ Radix.java
@@ -1,6 +1,7 @@
+public class Radix {
 public static int nth(int n, int col){
 return Math.abs((n / (int) Math.pow(10,col))
- % 10);
+ % 10);}
 
 public static int length(int n){
 
@@ -22,9 +23,47 @@ public static int length(int n){
           original.extend(buckets[i]);
       }
   }
- 
- 
- 
- 
- 
+
+
+  public static void radixSortSimple(SortableLinkedList data){
+    
+  SortableLinkedList[] buckets = new SortableLinkedList[10];
+  for(int i = 0; i < buckets.length; i++) {
+    buckets[i] = new SortableLinkedList();}
+//find MAX
+int mx = 0;
+for(int i = 0; i < data.size(); i++){
+  mx = Math.max(mx, length(data.get(i)));
 }
+
+  for (int i = 0; i < mx; i++){
+    for(int j=0; j<data.size(); j++){
+      int nv = data.remove(0);
+      buckets[nth(data.get(0), i)].add(nv);
+    }
+    merge(data, buckets);
+  }
+}
+
+  
+
+
+
+  public static void radixSort(SortableLinkedList data) {
+  SortableLinkedList positive = new SortableLinkedList();
+  SortableLinkedList negitive = new SortableLinkedList();
+    while(data.size() > 0) {
+        int firstint = data.remove(0);
+        if(firstint >= 0) {
+            positive.add(firstint);
+        } else {
+            negitive.add(firstint);}}
+      
+    radixSortSimple(positive);
+    radixSortSimple(negitive);
+    
+    while(negitive.size() > 0) {
+        data.add(negitive.remove(negitive.size() - 1));
+    }
+    data.extend(positive);
+}}
