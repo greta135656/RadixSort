cc609268d62f311ff1fd6d4279b7efcb95da224e
greta135656 <72942679+greta135656@users.noreply.github.com>
2021-01-13 23:33:37 +0000

    will run another tester to double check

--- Radix.java
+++ Radix.java
@@ -4,21 +4,17 @@ return Math.abs((n / (int) Math.pow(10,col))
  % 10);}
 
 public static int length(int n){
-
-  
       int count = 0;
       while (n != 0) {
           n = n / 10;
-          ++count;
+          count++;
       }
       return count;
   }
 
-  
-
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) 
-  {for(int i = 0; i < buckets.length; ++i) 
+  {for(int i = 0; i < buckets.length; i++) 
     {
           original.extend(buckets[i]);
       }
@@ -30,16 +26,17 @@ public static int length(int n){
   SortableLinkedList[] buckets = new SortableLinkedList[10];
   for(int i = 0; i < buckets.length; i++) {
     buckets[i] = new SortableLinkedList();}
+
 //find MAX
 int mx = 0;
 for(int i = 0; i < data.size(); i++){
-  mx = Math.max(mx, length(data.get(0)));
+  mx = Math.max(mx, length(data.get(i)));
 }
 
   for (int i = 0; i < mx; i++){
-    for(int j=0; j<data.size(); j++){
+    while(data.size()>0){
       int nv = data.remove(0);
-      buckets[nth(data.get(0), i)].add(nv);
+      buckets[nth(nv, i)].add(nv);
     }
     merge(data, buckets);
   }
